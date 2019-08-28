32d57b134a858a0d01ee4a037d094460b4ebf3ae
nihalsubandh1 <51986353+nihalsubandh1@users.noreply.github.com>
2019-08-28 16:57:34 +0530

    Update hack.js

--- hack.js
+++ hack.js
@@ -1,10 +1,5 @@
 
 var pubWindow = window.top || window;
 var pubDoc = pubWindow.document;
-var pubBody = pubDoc.body;
-var siteWidth = 1200;
-
-
-var header = pubDoc.querySelector('#globehead-container');
 
 
